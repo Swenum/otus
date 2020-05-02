228ddda46d32701b50ef121b9bb16864509f6b35
Swenum <wetoster@gmail.com>
2020-05-02 17:15:32 +0300

    Dz 4

--- Dz_4_Awk_Sed/scripts/parse-and-send-logs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-source /vagrant/config.ini
-export $(cut -d= -f1 /vagrant/config.ini)
-
-/vagrant/scripts/solo.sh /vagrant/scripts/process-logs.sh | mail -s "Logs report" "$MAILTO"
--- Dz_4_Awk_Sed/scripts/print-errors.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ERROR_LOG="$1"
-
-sed -nr 's/.*\[error\] [0-9]{1,10}#[0-9]{1,5}: \*[0-9]{1,10}(.*)\, client.*/\1/p' "$ERROR_LOG"
